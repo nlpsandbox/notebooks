5838b8147d6fec21e8f18497402b02a78dbfa83c
tschaffter <thomas.schaffter@gmail.com>
2021-11-04 14:07:29 -0700

    Finalize setu

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,12 @@ if [ "$1" = 'rstudio' ]; then
     # RStudio userconf.sh script runs RStudio as $USER
     exec /init
 elif [ "$1" = 'jupyter' ]; then
-  exec /init gosu $USER /opt/miniconda/envs/nlpsandbox/bin/jupyter-lab --port=8888 --no-browser --ip=0.0.0.0
+    # /init takes care of creating $USER. It also starts RStudio.
+    exec /init gosu $USER /opt/miniconda/envs/nlpsandbox/bin/jupyter-lab \
+        --notebook-dir /home/${USER}/nlpsandbox \
+        --port=8888 \
+        --no-browser \
+        --ip=0.0.0.0
 fi
 
 exec /init gosu $USER "$@"
