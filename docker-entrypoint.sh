de344b225203718605ea3a4f6aaabdeada90852a
tschaffter <thomas.schaffter@gmail.com>
2021-11-04 11:24:42 -0700

    Can start jupyter but permission issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,8 @@ set -e
 if [ "$1" = 'rstudio' ]; then
     # RStudio userconf.sh script runs RStudio as $USER
     exec /init
+elif [ "$1" = 'jupyter' ]; then
+  exec /init gosu $USER /opt/miniconda/envs/nlpsandbox/bin/jupyter-lab --port=8888 --no-browser --ip=0.0.0.0
 fi
 
 exec /init gosu $USER "$@"
